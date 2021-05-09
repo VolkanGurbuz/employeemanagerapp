b19a537d26f3830f3658ff8fa18a925eaaccc7f3
volkangurbuz <volkangurbuz16@gmail.com>
2021-05-09 21:27:52 +0200

    added angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from './employee';
+import {EmployeeService} from './employee.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit{
+  // @ts-ignore
+  public employees: Employee[];
+
+
+  constructor(private employeeService: EmployeeService) {}
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      // tslint:disable-next-line:no-shadowed-variable
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+    }
+    );
+  }
+
+
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee{
+
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: string;
+  imageURl: string;
+  employeeCode: string;
+
+}
